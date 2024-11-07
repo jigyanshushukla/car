e79442809f2cb3ea419d6273eb48d9dfe5f3fb23
Jigyanshu <jigyanshu>
2024-11-07 14:57:44 +0530

    rename front controller

--- app/Http/Controllers/frontController.php
+++ app/Http/Controllers/frontController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 
-class frontController extends Controller
+class FrontController extends Controller
 {
    public function index()
  {
